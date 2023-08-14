25a190695b310ccc0987dacdf1ef0977e63ce0e1
ReethuKeerthana <140586382+ReethuKeerthana@users.noreply.github.com>
2023-08-14 16:23:45 +0530

    Create todo.js

---
+++ todo-cli/todo.js
@@ -0,0 +1,48 @@
+const todoList = () => 
+  {
+  const all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = True;
+  };
+  const overdue = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((todo) => todo.dueDate < today);
+  };
+  const dueToday = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((todo) => todo.dueDate === today);
+  };
+  const dueLater = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((todo) => todo.dueDate > today);
+  };
+  const toDisplayableList = (list) => {
+    let displayList =" ";
+    list.forEach((todo) => {
+      const { title, dueDate, completed } = todo;
+      if (dueDate < today) {
+        displayList += `[ ] ${title} ${dueDate}\n`;
+      } else if (dueDate === today) {
+        displayList += `[${completed ? "x":"  "}] ${title}\n`;
+      } else {
+        displayList += `[ ] ${title} ${dueDate}\n`;
+      }
+    });
+    displayList = displayList.trim();
+    return displayList;
+  };
+  return 
+  {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+module.exports = todoList;
