3426eaadd65d7314b434fa6d67964944b4200c15
ReethuKeerthana <140586382+ReethuKeerthana@users.noreply.github.com>
2023-08-14 16:35:33 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,42 +1,49 @@
- /* eslint-disable no-undef */
-const todoList = () => 
-  {
+/* eslint-disable no-undef */
+const todoList = () => {
   const all = [];
   const add = (todoItem) => {
     all.push(todoItem);
   };
   const markAsComplete = (index) => {
-    all[index].completed = True;
+    all[index].completed = true;
   };
+
   const overdue = () => {
     const today = new Date().toISOString().split("T")[0];
     return all.filter((todo) => todo.dueDate < today);
   };
+
   const dueToday = () => {
     const today = new Date().toISOString().split("T")[0];
     return all.filter((todo) => todo.dueDate === today);
   };
+
   const dueLater = () => {
     const today = new Date().toISOString().split("T")[0];
     return all.filter((todo) => todo.dueDate > today);
   };
+
   const toDisplayableList = (list) => {
-    let displayList =" ";
+    // Format the To-Do list here, and return the output string
+    // as per the format given above.
+    let displayList = "";
     list.forEach((todo) => {
       const { title, dueDate, completed } = todo;
+
       if (dueDate < today) {
         displayList += `[ ] ${title} ${dueDate}\n`;
       } else if (dueDate === today) {
-        displayList += `[${completed ? "x":"  "}] ${title}\n`;
+        displayList += `[${completed ? "x" : " "}] ${title}\n`;
       } else {
         displayList += `[ ] ${title} ${dueDate}\n`;
       }
     });
     displayList = displayList.trim();
+
     return displayList;
   };
-  return 
-  {
+
+  return {
     all,
     add,
     markAsComplete,
