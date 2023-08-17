c1c629b3065e81140db602f1b4e50371346c460e
ReethuKeerthana <140586382+ReethuKeerthana@users.noreply.github.com>
2023-08-17 18:41:47 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -9,22 +9,45 @@ const todoList = () => {
   };
 
   const overdue = () => {
-    return all.filter(
-      (todo) => todo.dueDate < new Date().toLocaleDateString("en-CA")
-    );
+    const today = new Date().toISOString().split("T")[0];
+    const overdueItems = all.filter((todo) => todo.dueDate < today);
+    return overdueItems;
   };
 
   const dueToday = () => {
-    return all.filter(
-      (todo) => todo.dueDate === new Date().toLocaleDateString("en-CA")
-    );
+    const today = new Date().toISOString().split("T")[0];
+    const itemsDueToday = all.filter((todo) => todo.dueDate === today);
+    return itemsDueToday;
   };
 
   const dueLater = () => {
-    return all.filter(
-      (todo) => todo.dueDate > new Date().toLocaleDateString("en-CA")
-    );
+    const today = new Date().toISOString().split("T")[0];
+    const itemsDueLater = all.filter((todo) => todo.dueDate > today);
+    return itemsDueLater;
   };
+
+  const toDisplayableList = (list) => {
+    let output = "";
+    for (let i = 0; i < list.length; i++) {
+      const todo = list[i];
+      const status = todo.completed ? "x" : " ";
+      if (i === list.length - 1) {
+        if (todo.dueDate === today) {
+          output += `[${status}] ${todo.title}`;
+        } else {
+          output += `[${status}] ${todo.title} ${todo.dueDate}`;
+        }
+      } else {
+        if (todo.dueDate === today) {
+          output += `[${status}] ${todo.title}\n`;
+        } else {
+          output += `[${status}] ${todo.title} ${todo.dueDate}\n`;
+        }
+      }
+    }
+    return output;
+  };
+
   return {
     all,
     add,
@@ -32,11 +55,8 @@ const todoList = () => {
     overdue,
     dueToday,
     dueLater,
+    toDisplayableList,
   };
 };
 
 module.exports = todoList;
-
-// ####################################### #
-// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
-// ####################################### #
